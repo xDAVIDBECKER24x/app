a145d35d86834a87343225200bb1e533a30a9bc8
xDAVIDBECKER24x <drcarollo123@gmail.com>
2022-05-25 22:37:26 -0300

    primefaces final fixes

--- src/main/java/com/infotera/app/controller/ClienteController.java
+++ src/main/java/com/infotera/app/controller/ClienteController.java
@@ -1,26 +1,13 @@
 package com.infotera.app.controller;
 
-import java.util.Optional;
-
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-
 import com.infotera.app.model.Cliente;
-import com.infotera.app.repository.ClienteRepository;
 import com.infotera.app.service.IClienteService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Named
@@ -39,7 +26,7 @@ public class ClienteController {
         this.clientes = clienteService.getAllClientes();
     }
 
-    public String deleteCliente(Cliente cliente) {
+    public void deleteCliente(Cliente cliente) {
         clienteService.deleteCliente(cliente);
         clientes.remove(cliente);
     }
